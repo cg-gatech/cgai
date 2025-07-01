84b0231e0548d0e61f6b01ae6e826a18ad07c4e1
wrc042 <wrc0326@outlook.com>
2025-07-01 21:47:06 +0800

    update sdf shader

--- src/app/(tutorial)/tutorial/sdf-basis/fragment.glsl
+++ src/app/(tutorial)/tutorial/sdf-basis/fragment.glsl
@@ -15,60 +15,58 @@ uniform float iTime;
 uniform vec2 iResolution;
 uniform vec2 iMouse;
 
-float sdf_circle(vec2 p, vec2 c, float r)
-{
+float sdf_circle(vec2 p, vec2 c, float r) {
     return length(p - c) - r;
 }
 
 // reference: https://iquilezles.org/articles/distfunctions2d/, Box-exact
-float sdf_box( in vec2 p, float angle, in vec2 b )
-{
+float sdf_box(in vec2 p, float angle, in vec2 b) {
     mat2 rot = mat2(cos(-angle), sin(-angle), -sin(-angle), cos(-angle));
     p = rot * p;
 
-    vec2 d = abs(p)-b;
-    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+    vec2 d = abs(p) - b;
+    return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
 }
 
-void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{
-    vec2 p = (2.0 * fragCoord - iResolution.xy) / iResolution.y;    // p's range is between (-aspect_ratio,-1) to (+aspect_ratio,+1)
-    vec2 c = vec2(0.0, 0.0);
-    float r = 0.2;
-    
-    // circle
-    // float d = sdf_circle(p, c, r);
+vec3 vis_sdf_shader_toy(float d) {
+    vec3 color = (d > 0.0) ? vec3(0.9, 0.6, 0.3) : vec3(0.65, 0.85, 1.0);
+    color *= 1.0 - exp(-6.0 * abs(d));
+    color *= 0.8 + 0.2 * sin(100.0 * d);
+    color = mix(color, vec3(1.0), 1.0 - smoothstep(0.0, 0.01, abs(d)));
+    return color;
+}
 
-    // // rotating box
+vec3 vis_sdf(float d) {
+    vec3 color = (d > 0.0) ? vec3(0.6) : vec3(0.25);
+    float bandwidth = 0.08;
+    float half_bandwidth = bandwidth * 0.5;
+    float band = floor(d / bandwidth) * bandwidth;
+    color *= exp(-abs(band));
+
+    float linewidth = 0.006;
+    if(abs(d - band - half_bandwidth) > half_bandwidth - linewidth) {
+        color = mix(color, vec3(0.0), smoothstep(half_bandwidth - linewidth, half_bandwidth, abs(d - band - half_bandwidth)));
+    }
+    if(abs(d) < linewidth * 1.2) {
+        color = mix(color, vec3(1.0), 1.0 - smoothstep(0.0, linewidth * 1.2, abs(d)));
+    }
+    return color;
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec2 p = (2.0 * fragCoord - iResolution.xy) / iResolution.y;    // p's range is between (-aspect_ratio,-1) to (+aspect_ratio,+1)
+
+    // rotating box
     vec2 b = vec2(0.2, 0.3);
     float d = sdf_box(p, iTime, b);
 
     // our coloring implementation
-    vec3 color = vec3(0.0, 0.0, 0.0);
-
-	// ShaderToy coloring implementation
-    // color = (d > 0.0) ? vec3(0.9, 0.6, 0.3) : vec3(0.65, 0.85, 1.0);
-    // color *= 1.0 - exp(-6.0 * abs(d));
-    // color *= 0.8 + 0.2 * sin(100.0 * d);
-    // color = mix(color, vec3(1.0), 1.0 - smoothstep(0.0, 0.01, abs(d)));
-
-    color = (d > 0.0) ? vec3(0.6) : vec3(0.25);
-    float wfloor = 0.08;
-    float hwfloor = wfloor * 0.5;
-    float dfloor = floor(d / wfloor) * wfloor;
-    color *= exp(-1.0 * abs(dfloor));
-    float width = 0.006;
-    if (abs(d - dfloor - hwfloor) > hwfloor - width) {
-        color = mix(color, vec3(0.0), smoothstep(hwfloor - width, hwfloor, abs(d - dfloor - hwfloor)));
-    }
-    if (abs(d) < width * 1.2) {
-        color = mix(color, vec3(1.0), 1.0 - smoothstep(0.0, width * 1.2, abs(d)));
-    }
+    // vec3 color = vis_sdf_shader_toy(d);
+    vec3 color = vis_sdf(d);
 
     fragColor = vec4(color, 1.0);
 }
 
-void main()
-{
+void main() {
     mainImage(gl_FragColor, gl_FragCoord.xy);
 }
