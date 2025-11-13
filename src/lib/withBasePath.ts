c033bbebeb1f6ad51e1c516cd4a0950c3995acc5
CSK <73425927+cr2007@users.noreply.github.com>
2025-11-13 22:03:58 +0400

    fix(withBasePath): adds check to avoid double prefix

--- src/lib/withBasePath.ts
+++ src/lib/withBasePath.ts
@@ -1,5 +1,6 @@
 export const withBasePath = (path: string) => {
   const base = process.env.NEXT_PUBLIC_BASE_PATH || '';
   if (!path.startsWith('/')) path = '/' + path;
+  if (base && path.startsWith(base)) return path; // avoid double prefix
   return `${base}${path}`;
 };
