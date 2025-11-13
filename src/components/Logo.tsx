334d180d6ef7b064e9be700d8d8607387944e3be
CSK <73425927+cr2007@users.noreply.github.com>
2025-11-13 16:53:12 +0400

    fix(Logo.tsx): fixes base path for logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,16 @@
+import { withBasePath } from '@/lib/withBasePath';
 import Image from 'next/image';
 import React from 'react';
 
 const Logo: React.FC = () => {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
   return (
     <div className="absolute top-4 left-4 z-10">
       <a
         className="font-[family-name:var(--font-geist-mono)] flex items-center justify-center gap-2 text-background"
-        href="/"
+        href={withBasePath("/")}
         rel="noopener noreferrer"
       >
-        <Image src={`${basePath}/cgai_logo.png`} alt="CGAI logomark" width={20} height={20} />
+        <Image src={withBasePath("/cgai_logo.png")} alt="CGAI logomark" width={20} height={20} />
         CGAI
       </a>
     </div>
